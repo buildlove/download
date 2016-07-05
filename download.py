302ae38dfd61ec65e5819519acfd3a954527c789
李洋 <liyang@forensix.cn>
2016-07-05 15:58:21 +0800

    增加图形界面

--- download.py
+++ download.py
@@ -1,42 +1,91 @@
-#-*- coding:utf8 -*-
+#-*-coding=utf8-*-
+import sys
 import urllib
 import os
-
+import re
+from PyQt4 import QtGui, QtCore
 localPath = os.getcwd()
-#不存在该文件就创建该文件
-def opendir(filePath):
-  if not os.path.isdir(filePath):
-    os.mkdir(filePath)
 
-#下载进度条回调
-def callbackfunc(blocknum, blocksize, totalsize):
-    percent = 100.0 * blocknum * blocksize / totalsize
-    if percent > 100:
-        percent = 100
-    print "%.2f%%" % percent
+class OpenFile(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        QtGui.QMainWindow.__init__(self)
+        self.resize(600, 600)                    #窗口大小
+        self.setWindowTitle('OpenFile')          #窗口title
+        self.center()                            #窗口位置
+        self.textEdit = QtGui.QTextEdit()        #文本
+        self.statusTxt(self.textEdit)            #设置状态栏文本
+        self.setWindowIcon(QtGui.QIcon('icons/news.png')) #设置窗口图标
+        self.menubar()                           #菜单栏
 
-#下载只存在url的txt文件
-def onlyUrl(filePath):
-  opendir('test')
-  filePath = localPath + filePath
-  messages = open(filePath).read().split('\n')
-  count = 1
-  for message in messages:
-    if message:
-      local = localPath + '/test/index' + str(count) + '.html'
-      urllib.urlretrieve(message, local, callbackfunc)
-      count += 1
-      print u"addPage: " + message
+    # 状态栏
+    def statusTxt(self, text):
+        self.setCentralWidget(text)              #设置状态栏文本
+        self.statusBar()                         #状态栏
+        self.setFocus()
 
-def onlyAll(filePath):
-  filePath = localPath + filePath
-  messages1 = open(filePath).read().split('\n')
-  count = 1
-  for message in messages1[1:]:
-    if message:
-      arr = message.split(',')
-      urllib.urlretrieve(arr[0], arr[1], callbackfunc)
-      print u"下载存在路径url的txt文件：" + arr[0]
+    # 菜单栏
+    def menubar(self):
+        #子菜单
+        esc = QtGui.QAction(QtGui.QIcon('icons/exit.png'), 'Exit', self)
+        esc.setShortcut('Ctrl+Q')
+        esc.setStatusTip('exit the program')
+        esc.connect(esc, QtCore.SIGNAL('triggered()'), QtGui.qApp, QtCore.SLOT('quit()'))
 
-# onlyAll('/message1.txt')
-onlyUrl('/message.txt')
+        opt = QtGui.QAction(QtGui.QIcon('icons/news.png'),'Open', self)  #图标
+        opt.setShortcut('Ctrl+O')                                        #快捷键
+        opt.setStatusTip('Open new file')                                #标题
+        opt.connect(opt, QtCore.SIGNAL('triggered()'), self.readFile)    #opt部件绑定readFile函数
+        #创建菜单栏，把exit作为子菜单加入菜单栏
+        menubar = self.menuBar()
+        File = menubar.addMenu('&File')
+        File.addAction(opt)
+        File.addAction(esc)
+
+    # 下载进度条回调
+    def timerEvent(self, blocknum, blocksize, totalsize):
+        percent = 100.0 * blocknum * blocksize / totalsize
+        if percent > 100:
+            percent = 100
+
+    def uniqList(self, ids):
+        news_ids = []
+        for ids in ids:
+            if ids not in news_ids:
+                news_ids.append(ids)
+        return news_ids
+
+    # 读取文件
+    def readFile(self):
+        if not os.path.isdir('test'):
+            os.mkdir('test')
+        filename = QtGui.QFileDialog.getOpenFileName(self, 'Open file', './') or ''
+        File = open(filename, 'r')
+        data = File.read()
+        Urltxt = self.uniqList(data.split('\n'))
+        count = 0
+        for message in Urltxt:
+          if message:
+            self.textEdit.append(message)
+            x,y = os.path.split(message)
+            if y:
+                local = localPath + '/test/' + y
+            else:
+                local = localPath + '/test/index' + str(count) + '.html'
+            urllib.urlretrieve(message, local, self.timerEvent)
+            count += 1
+        self.textEdit.append(u"共有任务数: "+str(count))
+
+    # 移动主窗口到屏幕中心
+    def center(self):
+        screen = QtGui.QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width()-size.width())/2,(screen.height()-size.height())/2)
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    cd = OpenFile()
+    cd.show()
+    sys.exit(app.exec_())
+
+
